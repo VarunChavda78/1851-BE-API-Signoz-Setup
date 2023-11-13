d23d93751cdfa3c6485dc86be0dbc2ff6542f3c6
Sathiyapriya <sathiyapriya@pearlthoughts.com>
2023-11-13 15:31:02 +0530

    Added category API

--- src/category/controllers/category.controller.ts
+++ src/category/controllers/category.controller.ts
@@ -1,6 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CategoryService } from '../services/category.service';
+import { CategoryRepository } from '../repositories/category.repository';
+import { Category } from '../entities/category.entity';
 
-@Controller('category')
+@Controller({
+  version: '1',
+  path: 'category',
+})
 export class CategoryController {
-  constructor() {}
+  constructor(
+    private categoryService: CategoryService,
+    private categoryRepository: CategoryRepository,
+  ) {}
+
+  @Get('list')
+  async categoryList() {
+    const categories = await this.categoryRepository.getAll();
+    const data = await this.categoryService.getDetails(categories);
+    return { data: data };
+  }
+
+  @Get(':id')
+  async category(@Param('id') id: number) {
+    const category: Category = await this.categoryRepository.getById(id);
+    const data = {
+      id: category.id,
+      name: category.name,
+    };
+    return { data: data };
+  }
 }
--- src/category/services/category.service.ts
+++ src/category/services/category.service.ts
@@ -4,4 +4,15 @@ import { CategoryRepository } from '../repositories/category.repository';
 @Injectable()
 export class CategoryService {
   constructor(private repository: CategoryRepository) {}
+
+  async getDetails(categories) {
+    const data = [];
+    for (const category of categories) {
+      data.push({
+        id: category.id,
+        name: category.name,
+      });
+    }
+    return data;
+  }
 }
