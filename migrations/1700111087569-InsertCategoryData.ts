87b96378e6ab32aa60bc15ec4517bee2c1f44486
Sathiyapriya <sathiyapriya@pearlthoughts.com>
2023-12-11 18:33:58 +0530

    Change Category Data

--- migrations/1700111087569-InsertCategoryData.ts
+++ migrations/1700111087569-InsertCategoryData.ts
@@ -5,7 +5,7 @@ export class InsertCategoryData1700111087569 implements MigrationInterface {
   public async up(_queryRunner: QueryRunner): Promise<void> {
     await _queryRunner.query(`
       INSERT INTO category (name)
-      VALUES ('Accounting'), ('Banking/Financing'), ('Brokers'), ('Consulting/Development'), ('Events'), ('Legal'), ('Marketing'), ('Merchant Services'), ('Other Technology'), ('Public Relations'), ('Real Estate');
+      VALUES ('Accounting'), ('Banking / Financing'), ('Brokers'), ('Consulting / Development'), ('Events'), ('Legal'), ('Marketing'), ('Merchant Services'), ('Other Technology'), ('Public Relations'), ('Real Estate');
     `);
   }
 
