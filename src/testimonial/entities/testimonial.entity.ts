5cea1905ea7941389bf3299a659f48560954753b
Sathiyapriya <sathiyapriya@pearlthoughts.com>
2024-01-05 18:12:32 +0530

    Changed Testimonial data

--- src/testimonial/entities/testimonial.entity.ts
+++ src/testimonial/entities/testimonial.entity.ts
@@ -10,16 +10,16 @@ export class Testimonial {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ nullable: true })
   name: string;
 
-  @Column()
+  @Column({ nullable: true })
   designation: string;
 
-  @Column()
+  @Column({ nullable: true })
   title: string;
 
-  @Column()
+  @Column({ nullable: true })
   description: string;
 
   @Column({ nullable: true })
