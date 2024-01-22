0697ebe0b016e555762c974d6936577c73b524b7
poonkodi <poonkodi@pearlthoughts.com>
2024-01-22 15:18:56 +0530

    change origin in cors.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigin = 'https://1851dev.com';
+  app.use(
+    cors({
+      origin: allowedOrigin,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    }),
+  );
 
   app.enableVersioning({
     type: VersioningType.URI,
