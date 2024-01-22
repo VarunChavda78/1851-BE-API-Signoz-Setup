a397fc9148e9a791c03aed375bccbdc66e12b7f4
poonkodi <poonkodi@pearlthoughts.com>
2024-01-22 15:32:51 +0530

    disable cors.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const allowedOrigin = 'https://1851dev.com';
-  app.use(
-    cors({
-      origin: allowedOrigin,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    }),
-  );
-
   app.enableVersioning({
     type: VersioningType.URI,
   });
