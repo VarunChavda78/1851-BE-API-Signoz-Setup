c0605ec3a117215237769ac272acdbeb693191ea
poonkodi <poonkodi@pearlthoughts.com>
2024-01-22 14:58:42 +0530

    enable cors.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
 
   app.enableVersioning({
     type: VersioningType.URI,
