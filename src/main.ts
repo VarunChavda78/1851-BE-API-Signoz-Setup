923d7039c16186875018db05829b55afb96ae8b7
Sathiyapriya <sathiyapriya@pearlthoughts.com>
2024-01-22 15:50:37 +0530

    Enable Cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: configService.get('franchise.url'),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    optionsSuccessStatus: 200,
+    credentials: true,
+    allowedHeaders: 'Content-Type,X-Requested-With',
+  });
+
   app.enableVersioning({
     type: VersioningType.URI,
   });
