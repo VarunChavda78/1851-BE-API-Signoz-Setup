02723664d20d4bdd12ecd21d3e7af73c8cf81888
Sathiyapriya <sathiyapriya@pearlthoughts.com>
2024-01-30 16:22:40 +0530

    Removed unwanted files

--- src/power-ranking/power-ranking.module.ts
+++ src/power-ranking/power-ranking.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { PowerRanking } from './power-ranking.entity';
-import { PowerRankingService } from './services/power-ranking.service';
 import { PowerRankingRepository } from './repositories/power-ranking.repository';
-import { PowerRankingController } from './power-ranking.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PowerRanking])],
-  providers: [PowerRankingService, PowerRankingRepository, ConfigService],
-  controllers: [PowerRankingController],
-  exports: [PowerRankingService],
+  providers: [PowerRankingRepository, ConfigService],
+  controllers: [],
+  exports: [],
 })
 export class PowerRankingModule {}
--- src/power-ranking/services/power-ranking.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-
-@Injectable()
-export class PowerRankingService {
-  constructor(private readonly config: ConfigService) {}
-}
