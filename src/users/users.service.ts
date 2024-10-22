5cbfbf5651edc282d15510e85b98abfc69614fca
tosmim <tosmim@pearlthoughts.com>
2024-10-22 18:50:06 +0530

    DT-4141 Updated API with query and filters

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,57 +14,96 @@ export class UsersService {
 
   async findAll(filterDto?: FilterDto) {
     try {
-      const { page = 1, limit = 20, status, role } = filterDto;
-      const skip = (page - 1) * limit;
-      let usersQuery = this.usersRepository
-        .createQueryBuilder('registration')
-        .select();
-      let adminQuery = this.adminRepository
-        .createQueryBuilder('admin')
-        .select();
+      const { page = 1, limit = 20, status, role } = filterDto || {};
+      const pageNum = Number(page);
+      const limitNum = Number(limit);
+      const skip = (pageNum - 1) * limitNum;
 
-      if (status) {
-        usersQuery = usersQuery.andWhere('registration.status = :status', {
-          status,
-        });
+      let query;
+      let totalRecords = 0;
+      if (!role && !status) {
+        const adminQuery = this.adminRepository
+          .createQueryBuilder('admin')
+          .select(this.selectAdminFields());
+
+        const userQuery = this.usersRepository
+          .createQueryBuilder('registration')
+          .select(this.selectUserFields());
+
+        const [adminResults, userResults] = await Promise.all([
+          adminQuery.getMany(),
+          userQuery.getMany(),
+        ]);
+
+        let results = [...adminResults, ...userResults].sort(
+          (a, b) =>
+            new Date(b.created_date).getTime() -
+            new Date(a.created_date).getTime(),
+        );
+
+        totalRecords = results.length;
+
+        results = results.slice(skip, skip + limitNum);
+
+        const formattedData = this.formatData(results);
+
+        const pagination = this.commonService.getPagination(
+          totalRecords,
+          limitNum,
+          pageNum,
+        );
+
+        return { data: formattedData, pagination };
       }
 
-      if (role) {
-        switch (role) {
-          case 'brand':
-            usersQuery = usersQuery.andWhere(
-              'registration.brand_category_id > 0',
-            );
-            break;
-          case 'author':
-          case 'user':
-            usersQuery = usersQuery.andWhere('registration.user_type = :role', {
-              role,
-            });
-            break;
-          case 'admin':
-          case 'superadmin':
-            adminQuery = adminQuery.andWhere('admin.type = :role', { role });
-            break;
+      if (role === 'admin' || role === 'superadmin') {
+        query = this.adminRepository
+          .createQueryBuilder('admin')
+          .select(this.selectAdminFields());
+
+        if (role) {
+          query = query.andWhere('admin.type = :role', { role });
         }
-      }
-      const totalRecords =
-        (await usersQuery.getCount()) + (await adminQuery.getCount());
-      const [users, admins] = await Promise.all([
-        usersQuery.getMany(),
-        adminQuery.getMany(),
-      ]);
 
-      let combinedResults = [...users, ...admins];
-      combinedResults = combinedResults.slice(skip, skip + limit);
-      const formattedData = this.formatData(combinedResults);
-      const pagination = this.commonService.getPagination(
-        totalRecords,
-        page,
-        limit,
-      );
-      return { data: formattedData, pagination };
+        totalRecords = await query.getCount();
+        const results = await query.skip(skip).take(limit).getMany();
+        const formattedData = this.formatData(results);
+
+        const pagination = this.commonService.getPagination(
+          totalRecords,
+          limitNum,
+          pageNum,
+        );
+
+        return { data: formattedData, pagination };
+      } else {
+        query = this.usersRepository
+          .createQueryBuilder('registration')
+          .select(this.selectUserFields());
+
+        if (status) {
+          query = query.andWhere('registration.status = :status', { status });
+        }
+
+        if (role === 'brand') {
+          query = query.andWhere('registration.brand_category_id > 0');
+        } else if (role === 'author' || role === 'user') {
+          query = query.andWhere('registration.user_type = :role', { role });
+        }
+        totalRecords = await query.getCount();
+        const results = await query.skip(skip).take(limit).getMany();
+        const formattedData = this.formatData(results);
+
+        const pagination = this.commonService.getPagination(
+          totalRecords,
+          limit,
+          page,
+        );
+
+        return { data: formattedData, pagination };
+      }
     } catch (error) {
+      console.log(error);
       throw error;
     }
   }
@@ -72,10 +111,10 @@ export class UsersService {
   private formatData(data: any) {
     const response = data.map((user) => {
       let role;
-      if (user?.user_type) {
-        role = user.user_type;
-      } else if (user?.type) {
-        role = user.brand_category_id > 0 ? 'brand' : user.type;
+      if (user?.type) {
+        role = user.type;
+      } else if (user?.user_type) {
+        role = user.brand_category_id > 0 ? 'brand' : user.user_type;
       }
       return {
         id: user.id,
@@ -89,35 +128,35 @@ export class UsersService {
 
   private selectAdminFields() {
     return [
-      'id',
-      'first_name',
-      'last_name',
-      'company',
-      'email',
-      'phone',
-      'user_name',
-      'type',
-      'registration_date',
-      'created_date',
-      'photo',
+      'admin.id',
+      'admin.first_name',
+      'admin.last_name',
+      'admin.company',
+      'admin.email',
+      'admin.phone',
+      'admin.user_name',
+      'admin.type',
+      'admin.registration_date',
+      'admin.created_date',
+      'admin.photo',
     ];
   }
 
   private selectUserFields() {
     return [
-      'id',
-      'first_name',
-      'last_name',
-      'company',
-      'email',
-      'phone',
-      'user_name',
-      'user_type',
-      'registration_date',
-      'created_date',
-      'photo',
-      'brand_category_id',
-      'status',
+      'registration.id',
+      'registration.first_name',
+      'registration.last_name',
+      'registration.company',
+      'registration.email',
+      'registration.phone',
+      'registration.user_name',
+      'registration.user_type',
+      'registration.registration_date',
+      'registration.created_date',
+      'registration.photo',
+      'registration.brand_category_id',
+      'registration.status',
     ];
   }
 }
