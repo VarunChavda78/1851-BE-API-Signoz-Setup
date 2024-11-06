cb64aec6eed53eda90846d90cd0d48bcfb3079b3
logeshpearl <logesh@pearlthoughts.com>
2024-11-06 19:29:15 +0530

    DT-4177 added error logs

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -1,17 +1,26 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import {
   S3Client,
   PutObjectCommand,
   DeleteObjectCommand,
 } from '@aws-sdk/client-s3';
 import { ConfigService } from '@nestjs/config';
+import { RollbarLogger } from 'nestjs-rollbar';
 
 @Injectable()
 export class S3Service {
   private s3Client: S3Client;
   private bucketName: string;
+  private logger = new Logger(S3Service.name);
 
-  constructor(private configservice: ConfigService) {
+  constructor(
+    private configservice: ConfigService,
+    private readonly rollbar: RollbarLogger,
+  ) {
     this.bucketName = this.configservice.get('aws.bucketName');
     this.s3Client = new S3Client({
       region: this.configservice.get('aws.region'),
@@ -29,12 +38,13 @@ export class S3Service {
   async uploadFile(
     file: any,
     path: string = '',
-  ): Promise<{ url: string; imagePath: string, message: string}> {
+  ): Promise<{ url: string; imagePath: string; message: string }> {
     if (path && !path.endsWith('/')) {
       path += '/';
     }
-
     const key = `${path}${Date.now()}_${file.originalname}`;
+    const fileUrl = `${this.configservice.get('s3.imageUrl')}/${key}`;
+    this.logger.debug(`key: ${key}, fileUrl: ${fileUrl}`);
 
     const command = new PutObjectCommand({
       Bucket: this.bucketName,
@@ -45,17 +55,19 @@ export class S3Service {
 
     try {
       await this.s3Client.send(command);
-      const fileUrl = `${this.configservice.get('s3.imageUrl')}/${key}`;
+
       return {
         message: `File uploaded successfully`,
         url: fileUrl,
         imagePath: key,
       };
     } catch (error) {
+      this.logger.error(`Could not upload file: ${error.message}`, error.stack);
+      this.rollbar.warning('Error in upload File method', error);
       return {
         message: `Could not upload file: ${error.message}`,
-        url: `${this.configservice.get('s3.imageUrl')}/${key}`,
-        imagePath: key
+        url: fileUrl,
+        imagePath: key,
       };
     }
   }
