100a11fcb8dc2332b7195b91b827aa290a3eea64
poonkodi <poonkodi@pearlthoughts.com>
2024-11-06 15:48:10 +0530

    change firebase config.

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { EnvironmentConfigService } from 'src/shared/config/environment-config.service';
 import { EmailService } from './email.service';
 import * as admin from 'firebase-admin';
+import { initializeFirebaseApp, getFirebaseApp } from './firebase.config';
+
 // import * as serviceAccount from './gcp-auth.json';
 
 // admin.initializeApp({
@@ -18,25 +20,18 @@ export class EmailController {
     private service: EmailService,
   ) {}
 
+  async onModuleInit() {
+    await initializeFirebaseApp(this.config);
+  }
   @Post('forgot-password')
   async forgotPassword(@Body() request: any) {
     try {
-      const url = `${this.config.getAwsS3Url()}/static/gcp-auth.json`;
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const serviceAccount = await response.json();
+      const app = getFirebaseApp();
 
-      admin.initializeApp({
-        credential: admin.credential.cert(
-          serviceAccount as admin.ServiceAccount,
-        ),
-      });
       const subject = 'Reset your password for 1851 Franchise';
       const tenantId = this.config.getTenantId();
       const emailId = request?.email;
-      const tenantAuth = admin.auth().tenantManager().authForTenant(tenantId);
+      const tenantAuth = app.auth().tenantManager().authForTenant(tenantId);
       const link = await tenantAuth.generatePasswordResetLink(emailId);
       const content = `<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n    <title>Admin Password Reset Notice</title>\n    <style>\n        body {\n            font-family: Arial, sans-serif;\n            line-height: 1.6;\n            color: #333;\n            max-width: 600px;\n            margin: 0 auto;\n            padding: 20px;\n        }\n        h4 {\n            margin-bottom: 20px;\n        }\n        p {\n            margin-bottom: 15px;\n        }\n        ol {\n            margin-left: 20px;\n            margin-bottom: 15px;\n        }\n        .signature {\n            margin-top: 20px;\n        }\n    </style>\n</head>\n<body>\n    <h4>Hello,</h4>\n    \n    <p>Follow this link to reset your 1851 Franchise password for your ${emailId} account.
   </p>\n    \n    <p><a href="${link}" target="_blank">${link}</a></p>\n    <p>\n      \n    <p>If you didn’t ask to reset your password, you can ignore this email.</p>\n    \n    <p class="signature">Best Regards,<br>1851 Franchise Team</p>\n</body>\n</html>`;
@@ -51,7 +46,7 @@ export class EmailController {
       await this.service.sendEmail(email);
       return { message: 'Email Sent Successfully' };
     } catch (error) {
-      console.error('Error generating password reset link:', error);
+      throw new Error(`Error generating password reset link: ${error}`);
     }
   }
 }
