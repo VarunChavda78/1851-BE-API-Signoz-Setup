878efe51d74a8b85dc5aa89b2a3704bcc2e2a135
shriyaPT <shriya@pearlthoughts.com>
2024-11-14 13:38:26 +0530

    create api for navigation-menu

--- src/navigation-menu/navigation-menu.controller.ts
+++ src/navigation-menu/navigation-menu.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Post,Body } from '@nestjs/common';
+import {NavigationCreateDto} from './dtos/navigation-create-dto'
+import  {NavigationMenuService} from './navigation-menu.service'
 
 @Controller('navigation-menu')
-export class NavigationMenuController {}
+export class NavigationMenuController {
+    constructor(private readonly NavigationMenuService: NavigationMenuService) {}
+
+  @Post('create')
+  async create(@Body() NavigationCreateDto: NavigationCreateDto) {
+    return await this.NavigationMenuService.create(NavigationCreateDto);
+  }
+}
+
--- src/navigation-menu/navigation-menu.service.ts
+++ src/navigation-menu/navigation-menu.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { NavigationMenu } from 'src/mysqldb/entities/customize-user-sidebar.entity';
 import { Repository } from 'typeorm';
+import {NavigationCreateDto} from './dtos/navigation-create-dto'
+
 
 @Injectable()
 export class NavigationMenuService{
@@ -9,4 +11,9 @@ export class NavigationMenuService{
         @InjectRepository(NavigationMenu, 'mysqldb')
         private navigationmenuRepository: Repository<NavigationMenu>
     ){}
+
+    async create(NavigationCreateDto: NavigationCreateDto): Promise<NavigationMenu> {
+        const franchiseInfo = this.navigationmenuRepository.create(NavigationCreateDto);
+        return await this.navigationmenuRepository.save(franchiseInfo);
+      }
 }
