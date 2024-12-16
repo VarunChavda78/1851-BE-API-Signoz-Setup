ef2fccc6fdd43c440b695a291837c96c43269ceb
shriyaPT <shriya@pearlthoughts.com>
2024-12-16 13:47:00 +0530

    added changes

---
+++ src/migrations/1733998927093-Landing2.ts
@@ -0,0 +1,152 @@
+import { MigrationInterface, QueryRunner,TableCheck,Table ,TableForeignKey} from "typeorm";
+
+export class Landing21733998927093 implements MigrationInterface {
+    name = 'Landing21733998927093'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        // ==== landing_page_customisation ====
+        const landingPageCustomisationExists = await queryRunner.hasTable('landing_page_customisation');
+        if (!landingPageCustomisationExists) {
+            await queryRunner.createTable(new Table({
+                name: "landing_page_customisation",
+                columns: [
+                    {
+                        name: "id",
+                        type: "serial", 
+                        isPrimary: true,
+                    },
+                    {
+                        name: "brandId",
+                        type: "int",
+                        isNullable: false,
+                    },
+                    {
+                        name: "sectionId",
+                        type: "int",
+                        isNullable: false,
+                    },
+                    {
+                        name: "content",
+                        type: "json",
+                        isNullable: false,
+                    },
+                    {
+                        name: "createdAt",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                    },
+                    {
+                        name: "updatedAt",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                    },
+                    {
+                        name: "createdBy",
+                        type: "int",
+                        isNullable: false,
+                    },
+                    {
+                        name: "updatedBy",
+                        type: "int",
+                        isNullable: false,
+                    },
+                    {
+                        name: "pageId",
+                        type: "int",
+                        isNullable: true, // Initially nullable for existing data
+                    },
+                ],
+                foreignKeys: [
+                    new TableForeignKey({
+                        columnNames: ["sectionId"],
+                        referencedColumnNames: ["id"],
+                        referencedTableName: "landing_page_section",
+                        onDelete: "CASCADE",
+                    }),
+                    new TableForeignKey({
+                        columnNames: ["pageId"],
+                        referencedColumnNames: ["id"],
+                        referencedTableName: "landing_pages",
+                        onDelete: "CASCADE",
+                    }),
+                ],
+            }), true);
+          
+        }
+
+        // ==== landing_page_publish ====
+        const landingPagePublishExists = await queryRunner.hasTable('landing_page_publish');
+        if (!landingPagePublishExists) {
+            await queryRunner.createTable(new Table({
+                name: 'landing_page_publish',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'serial', 
+                        isPrimary: true,
+                    },
+                    {
+                        name: 'brandId',
+                        type: 'int',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'brandSlug',
+                        type: 'varchar',
+                        length: '255',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'status',
+                        type: 'boolean',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'domainType',
+                        type: 'int',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'domain',
+                        type: 'varchar',
+                        length: '255',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'createdAt',
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP"
+                    },
+                    {
+                        name: 'updatedAt',
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP"
+                    },
+                    {
+                        name: 'createdBy',
+                        type: 'int',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'updatedBy',
+                        type: 'int',
+                        isNullable: false,
+                    },
+                ],
+                checks: [
+                    new TableCheck({
+                        name: 'check_domain_type',
+                        expression: `"domainType" IN (1, 2)`,
+                    }),
+                ],
+            }), true);
+
+        }
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('landing_page_publish', true);
+        await queryRunner.dropTable('landing_page_customisation', true);
+    }
+
+}
