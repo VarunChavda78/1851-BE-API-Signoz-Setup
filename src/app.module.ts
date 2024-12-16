8ef5a87b4ab9c0f52ab3959a1e5f8ab401b1bca7
Vamshi Goud Kesari <120409185+pt-vamshi@users.noreply.github.com>
2024-12-16 17:08:54 +0530

    Revert "added site log get api"

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,6 @@ import { EmailModule } from './email/email.module';
 import { S3Module } from './s3/s3.module';
 import { LandingPageModule } from './landing-page/landing-page.module';
 import { NavigationMenuModule } from './navigation-menu/navigation-menu.module';
-import { SiteLogModule } from './site-log/site-log.module';
 
 @Module({
   imports: [
@@ -34,7 +33,6 @@ import { SiteLogModule } from './site-log/site-log.module';
     S3Module,
     LandingPageModule,
     NavigationMenuModule,
-    SiteLogModule
   ],
   controllers: [AppController],
   providers: [AppService],
