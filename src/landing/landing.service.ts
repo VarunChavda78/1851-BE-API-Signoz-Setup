6f3f312b49d266843f6060c706c30ade541d2e74
manic56 <manikandan@pearlthoughts.com>
2024-12-23 16:55:02 +0530

    DT-4385 Add create api

--- src/landing/landing.service.ts
+++ src/landing/landing.service.ts
@@ -41,30 +41,30 @@ export class LandingService {
     }));
   }
 
-  // async createPage(
-  //   slug: string,
-  //   createPageDto: { name: string; templateId: number },
-  // ) {
-  //   const brand = await this.usersService.getBrandIdBySlug(slug);
-  //   if (!brand) {
-  //     throw new Error(`Brand not found for slug: ${slug}`);
-  //   }
+  async createPage(
+    slug: string,
+    createPageDto: { name: string; templateId: number },
+  ) {
+    const brand = await this.usersService.getBrandIdBySlug(slug);
+    if (!brand) {
+      throw new Error(`Brand not found for slug: ${slug}`);
+    }
 
-  //   const timestamp = new Date();
+    const timestamp = new Date();
 
-  //   const newPage = this.lpPageRepository.create({
-  //     brandId: brand.id,
-  //     name: createPageDto.name,
-  //     brandSlug: slug,
-  //     templateId: createPageDto.templateId,
-  //     status: PageStatus.DRAFT,
-  //     updatedBy: 1,
-  //     createdBy: 1,
-  //     createdAt: timestamp,
-  //     updatedAt: timestamp,
-  //     deletedAt: null,
-  //   });
+    const newPage = this.lpPageRepository.create({
+      brandId: brand.id,
+      name: createPageDto.name,
+      brandSlug: slug,
+      templateId: createPageDto.templateId,
+      status: PageStatus.DRAFT,
+      updatedBy: 1,
+      createdBy: 1,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+      deletedAt: null,
+    });
 
-  //   return await this.lpPageRepository.save(newPage);
-  // }
+    return await this.lpPageRepository.save(newPage);
+  }
 }
