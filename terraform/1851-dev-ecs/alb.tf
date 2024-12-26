b87e8b4abf749f77e1e51f3b2c6daed941265c5c
pt-vamshi <vamshi@pearlthoughts.com>
2024-12-26 14:09:39 +0530

    added health check

--- terraform/1851-dev-ecs/alb.tf
+++ terraform/1851-dev-ecs/alb.tf
@@ -18,6 +18,15 @@ resource "aws_lb_target_group" "ecs" {
   lifecycle {
     create_before_destroy = true
   }
+
+  health_check {
+    path                 = "/v1"
+    healthy_threshold    = 5
+    unhealthy_threshold  = 2
+    timeout              = 5
+    interval             = 30
+    matcher              = "200"  # has to be HTTP 200 or fails
+  }
 }
 
 resource "aws_lb_listener_rule" "static" {
