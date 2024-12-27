fc3b4209f71246f93cd5835d27f651b48a745d6b
pt-vamshi <vamshi@pearlthoughts.com>
2024-12-27 11:35:54 +0530

    fix terraform code

--- terraform/1851-prod-ecs/vpc-data.tf
+++ terraform/1851-prod-ecs/vpc-data.tf
@@ -4,20 +4,43 @@ data "aws_vpc" "vpc" {
     name   = "tag:Name"
     values = [var.vpc_name]
   }
+
+
 }
 
 data "aws_subnets" "private_subnets_id" {
 
   filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
+
+  filter {
     name   = "tag:Type"
     values = ["private"]
   }
+
+  filter {
+    name   = "tag:ManagedBy"
+    values = ["terraform"]
+  }
+
 }
 data "aws_subnets" "public_subnets_id" {
 
   filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.vpc.id]
+  }
+
+  filter {
     name   = "tag:Type"
     values = ["public"]
+  }
+
+  filter {
+    name   = "tag:ManagedBy"
+    values = ["terraform"]
   }
 
 }
