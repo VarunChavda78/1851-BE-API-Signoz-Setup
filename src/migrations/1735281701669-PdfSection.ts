66d00cc36549402e496dcbcde4c2bfdcfb5be3f9
poonkodi <poonkodi@pearlthoughts.com>
2024-12-27 13:18:34 +0530

    download pdf section migtation.

---
+++ src/migrations/1735281701669-PdfSection.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class PdfSection1735281701669 implements MigrationInterface {
+  name = 'PdfSection1735281701669';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            INSERT INTO landing_page_section (id,name, slug)
+            VALUES 
+            (55,'Download PDF', 't2-download-pdf')
+            ON CONFLICT (slug) DO NOTHING;
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    //
+  }
+}
