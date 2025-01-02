006d86b68e977cbe421d4372dce7aed9cdde5946
Pradeep1816 <pradeepmahto7991@gmail.com>
2025-01-03 00:34:18 +0530

    change datatypes

--- src/migrations/1735841711434-lookingFor.ts
+++ src/migrations/1735841711434-lookingFor.ts
@@ -1,15 +1,16 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class LookingFor1735633589194 implements MigrationInterface {
-    name = 'LookingFor1735633589194'
+export class LookingFor1735841711434 implements MigrationInterface {
+    name = 'LookingFor1735841711434'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `ALTER TABLE landing_page_leads ADD lookingFor int NOT NULL DEFAULT 1;`,
+            `ALTER TABLE landing_page_leads ADD lookingFor varchar NOT NULL;`,
           );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        
     }
 
 }
