d4b88dfb8cb812cf858493564d36d0d46fa0d0a3
manic56 <manikandan@pearlthoughts.com>
2025-01-02 11:07:56 +0530

    Add T3 sections

---
+++ src/migrations/1735794820759-AddT3Sections.ts
@@ -0,0 +1,28 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddT3Sections1704185600001 implements MigrationInterface {
+  name = 'AddT3Sections1704185600001';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      INSERT INTO lp_sections (id, "lpTemplatePageId", name, slug)
+      VALUES 
+      (56, 6, 'T3 Form', 't3-form') 
+      (57, 6, 'T3 Header', 't3-header'),
+      (58, 6, 'T3 Hero', 't3-hero'),
+      (59, 6, 'T3 Quick Facts', 't3-quickFacts'),
+      (60, 6, 'T3 Franchising 101', 't3-franchising101'),
+      (61, 6, 'T3 What Client Are Saying', 't3-whatClientAreSaying'),
+      (62, 6, 'T3 Why Us', 't3-why-us'),
+      (63, 6, 'T3 Footer', 't3-footer'),
+      (64, 6, 'T3 Meta', 't3-meta')
+      ON CONFLICT (id) DO NOTHING;
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      DELETE FROM lp_sections WHERE id IN (56, 57, 58, 59, 60, 61, 62, 63, 64);
+    `);
+  }
+}
