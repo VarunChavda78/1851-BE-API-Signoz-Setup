b8447d3b34b8033802391154b8f9a1a293249771
Vamshi Goud Kesari <120409185+pt-vamshi@users.noreply.github.com>
2025-04-23 11:51:25 +0530

    Update prod.sh

--- env/prod.sh
+++ env/prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-1:580880756845:secret:1851-Admin-BE-API-Prod-hilfCK --profile prod| \
 jq -r '.SecretString' | \
-jq -r 'to_entries|map("\(.key) = \(.value|tostring)")|.[]' > .env.production.local
+jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' > .env.production.local
