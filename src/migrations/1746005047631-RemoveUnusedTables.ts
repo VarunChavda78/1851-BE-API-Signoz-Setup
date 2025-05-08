be8dbac06c9e038dbadd820f46e723fc6dafec75
tosmim <tosmim@pearlthoughts.com>
2025-05-08 10:19:58 +0530

    DT-5006 Removed unused tables

--- src/migrations/1746005047631-RemoveUnusedTables.ts
+++ src/migrations/1746005047631-RemoveUnusedTables.ts
@@ -4,13 +4,22 @@ export class RemoveUnusedTables1746005047631 implements MigrationInterface {
   name = 'RemoveUnusedTables1746005047631';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "landing_page"`);
-    await queryRunner.query(`DROP TABLE "landing_page_customisation"`);
-    await queryRunner.query(`DROP TABLE "landing_page_leads"`);
-    await queryRunner.query(`DROP TABLE "landing_page_publish"`);
-    await queryRunner.query(`DROP TABLE "landing_page_section"`);
-    await queryRunner.query(`DROP TABLE "landing_pages"`);
-    await queryRunner.query(`DROP TABLE "landing_templates"`);
+    const tables = [
+      'landing_page',
+      'landing_page_customisation',
+      'landing_page_leads',
+      'landing_page_publish',
+      'landing_page_section',
+      'landing_pages',
+      'landing_templates',
+    ];
+
+    for (const table of tables) {
+      const exists = await queryRunner.hasTable(table);
+      if (exists) {
+        await queryRunner.query(`DROP TABLE "${table}"`);
+      }
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
